2403d1045b22818a18ec1ba9a32edaa72e66b3cf
dmquinn <danielmurphyberling@gmail.com>
2021-07-27 13:34:37 +0200

    changed config vars for deployment

--- server/server.js
+++ server/server.js
@@ -87,17 +87,17 @@ app.post("/login", (req, res) => {
 	spotifyApi.setAccessToken(code);
 });
 
-// if ((process.env.NODE_ENV = "production")) {
-app.use(express.static(path.join(__dirname, "../build")));
+if ((process.env.NODE_ENV = "production")) {
+	app.use(express.static(path.join(__dirname, "./build")));
 
-app.get("*", (req, res) =>
-	res.sendFile(path.resolve(__dirname, "build", "index.html"))
-);
-// } else {
-// 	app.get("/", (req, res) => {
-// 		res.send("API is running....");
-// 	});
-// }
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "build", "index.html"))
+	);
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running....");
+	});
+}
 
 app.listen(5000, () => {
 	console.log("listening on port 5000");
